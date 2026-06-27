22589895c41527d4f0df7b26d2cd60c52afca58d
LotusReichhart <reichhartlotus@gmail.com>
2026-06-27 22:40:47 +0700

    upgrade version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.lotusreichhart.colorscan"
         minSdk = 24
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
